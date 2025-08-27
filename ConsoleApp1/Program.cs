e26a37b7db586f778e117115603c2f3962722540
JCruz6725 <john.cruz6725@gmail.com>
2025-08-27 17:00:12 -0500

    .

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using ConsoleApp1Client;
+using Grpc.Net.Client;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1 {
+    internal class Program {
+        static async Task Main(string[] args) {
+
+            Console.WriteLine("Press any key after grpc service boots...");
+            Console.ReadKey();
+
+
+            string addr = "https://localhost:7158";
+
+
+            using var channel = GrpcChannel.ForAddress(addr);
+            var client = new Greeter.GreeterClient(channel);
+
+            var t  = await client.SayHelloAsync(new(){ Name = "(from client)"});
+
+            Console.WriteLine(t.Message);
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
